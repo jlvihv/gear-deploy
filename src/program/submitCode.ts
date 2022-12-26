e4f2a3e5838ef9d537acf4c9c25d38dcea762774
jlvihv <vihv@qq.com>
2022-12-26 21:44:58 +0800

    update

--- src/program/submitCode.ts
+++ src/program/submitCode.ts
@@ -1,8 +1,9 @@
-import { GearApi, GearKeyring } from '@gear-js/api';
+import { GearApi, GearKeyring, getWasmMetadata } from '@gear-js/api';
 import { readFileSync, writeFileSync } from 'fs';
-import { PATH_TO_OPT } from '../config';
+import { PATH_TO_OPT, PATH_TO_META } from '../config';
 
 let id = '';
+let metaString = '';
 
 const uploadCode = async () => {
     const api = await GearApi.create({
@@ -14,10 +15,17 @@ const uploadCode = async () => {
     console.log(`User logined address: ${user.address}`);
 
     const code = readFileSync(PATH_TO_OPT);
+    const metaFile = readFileSync(PATH_TO_META);
 
     const { codeHash } = await api.code.upload(code);
 
     console.log(`CodeHash: ${codeHash}\n`);
+
+    const meta = await getWasmMetadata(metaFile);
+    let metaInfo = JSON.stringify(meta, undefined, 2);
+    console.log(`Meta: ${metaInfo}`);
+    metaString = meta.types.toString();
+    console.log(`MetaString: ${metaString}`);
 
     try {
         return await new Promise((resolve, reject) => {
@@ -45,6 +53,7 @@ uploadCode()
     .then(() => {
         console.log(`ProgramID: ${id}\n`);
         writeFileSync('programID.txt', `REACT_APP_PROGRAM_ID=${id}`);
+        writeFileSync('metaString.txt', `REACT_APP_META_TYPES=${metaString}`);
         process.exit(0)
     })
     .catch((error) => {
