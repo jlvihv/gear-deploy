049c352790295860be1f1a651fd7bc026df497fd
jlvihv <vihv@qq.com>
2022-12-26 18:06:57 +0800

    change file name

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const PATH_TO_OPT = '../hello-world/target/wasm32-unknown-unknown/release/nft.opt.wasm';
-export const PATH_TO_META = '../hello-world/target/wasm32-unknown-unknown/release/nft.meta.wasm';
+export const PATH_TO_OPT = '../hello-world/target/wasm32-unknown-unknown/release/hello_world.opt.wasm';
+export const PATH_TO_META = '../hello-world/target/wasm32-unknown-unknown/release/hello_world.meta.wasm';
